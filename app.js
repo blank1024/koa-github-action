bbf95a49bb0f42abea58ccb0796558278ebc49d1
jawnwa22 <jawnwa22@gmail.com>
2019-11-22 19:24:56 +0800

    init file

---
+++ app.js
@@ -0,0 +1,17 @@
+import koa from 'koa';
+import logger from 'koa-logger';
+
+
+const app = new koa();
+const port = 4000;
+
+const hello = (ctx) => {
+    ctx.body = "hello";
+}
+
+app.use(logger());
+app.use(hello);
+
+app.listen(port, () => {
+    console.log(`✅  The server is running at http://localhost:${port}/`);
+})
